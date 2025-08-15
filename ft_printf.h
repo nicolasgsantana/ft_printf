438811f120c874027fc6633d25834cd1324df9d6
nicolasgsantana <nicolas07san@gmail.com>
2025-08-15 10:13:05 -0300

    fix ft_printf prototy and include unistd.h

--- ft_printf.h
+++ ft_printf.h
@@ -6,16 +6,17 @@
 /*   By: nicolas <nicolas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/07 15:31:25 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/08/14 14:00:53 by nicolas          ###   ########.fr       */
+/*   Updated: 2025/08/15 09:18:18 by nicolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include "libft/libft.h"
-#include <stdarg.h>
+# include "libft/libft.h"
+# include <stdarg.h>
+# include <unistd.h>
 
-int	ft_printf(const char *, ...);
+int	ft_printf(const char *format, ...);
 
 #endif
