b84451be2c8bebd621fcc4d5fedd48a2b4dd396d
nicolasgsantana <nicolas07san@gmail.com>
2025-08-21 13:14:31 -0300

    add utils for ft_printf

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/07 15:31:25 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/08/20 13:51:40 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/08/21 13:10:08 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,10 @@ int		ft_printf(const char *format, ...);
 char	*ft_itohex(uintptr_t n, bool uppercase);
 char	*ft_ptrtohex(void *ptr);
 char	*ft_uitoa(unsigned int n);
+int		printfchr(va_list *args);
+int		printfstr(va_list *args);
+int		printfint(va_list *args, bool uint);
+int		printfhex(va_list *args, bool uppercase);
+int		printfptr(va_list *args);
 
 #endif
