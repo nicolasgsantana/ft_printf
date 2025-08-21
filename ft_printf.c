6f1f171af5ec9409934307a2d41dfc62f4aae3f8
nicolasgsantana <nicolas07san@gmail.com>
2025-08-21 13:15:15 -0300

    update printflag to use utils functions

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/07 15:30:57 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/08/21 11:19:13 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/08/21 13:09:13 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,72 +14,20 @@
 
 static void	printflag(va_list *args, char flag, int *len)
 {
-	char	*arg;
-
 	if (flag == 'c')
-	{
-		*len += 1;
-		ft_putchar_fd(va_arg(*args, int), STDOUT_FILENO);
-	}
+		*len += printfchr(args);
 	else if (flag == 's')
-	{
-		arg = va_arg(*args, char *);
-		if (!arg)
-		{
-			ft_putstr_fd("(null)", STDOUT_FILENO);
-			*len += 6;
-		}
-		else
-		{
-			*len += ft_strlen(arg);
-			ft_putstr_fd(arg, STDOUT_FILENO);
-		}
-	}
+		*len += printfstr(args);
 	else if (flag == 'p')
-	{
-		arg = ft_ptrtohex(va_arg(*args, void *));
-		if (!arg)
-		{
-			ft_putstr_fd("(nil)", STDOUT_FILENO);
-			*len += 5;
-		}
-		else
-		{
-			*len += ft_strlen(arg);
-			*len += 2;
-			ft_putstr_fd("0x", STDOUT_FILENO);
-			ft_putstr_fd(arg, STDOUT_FILENO);
-		}
-		free(arg);
-	}
+		*len += printfptr(args);
 	else if (flag == 'd' || flag == 'i')
-	{
-		arg = ft_itoa(va_arg(*args, int));
-		*len += ft_strlen(arg);
-		ft_putstr_fd(arg, STDOUT_FILENO);
-		free(arg);
-	}
+		*len += printfint(args, false);
 	else if (flag == 'u')
-	{
-		arg = ft_uitoa(va_arg(*args, unsigned int));
-		*len += ft_strlen(arg);
-		ft_putstr_fd(arg, STDOUT_FILENO);
-		free(arg);
-	}
+		*len += printfint(args, true);
 	else if (flag == 'x')
-	{
-		arg = ft_itohex(va_arg(*args, unsigned int), false);
-		*len += ft_strlen(arg);
-		ft_putstr_fd(arg, STDOUT_FILENO);
-		free(arg);
-	}
+		*len += printfhex(args, false);
 	else if (flag == 'X')
-	{
-		arg = ft_itohex(va_arg(*args, unsigned int), true);
-		*len += ft_strlen(arg);
-		ft_putstr_fd(arg, STDOUT_FILENO);
-		free(arg);
-	}
+		*len += printfhex(args, true);
 	else if (flag == '%')
 	{
 		*len += 1;
@@ -89,8 +37,8 @@ static void	printflag(va_list *args, char flag, int *len)
 
 int	ft_printf(const char *format, ...)
 {
-	int	i;
-	int	len;
+	int		i;
+	int		len;
 	va_list	args;
 
 	va_start(args, format);
